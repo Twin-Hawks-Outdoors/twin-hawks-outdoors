1c7c136a609012499e5d730bd1190272064c083d
Luke Miller <lukem1293@gmail.com>
2022-03-07 22:13:12 -0600

    started working on textwithimage component

--- web/src/components/TextWithImage.tsx
+++ web/src/components/TextWithImage.tsx
@@ -1,8 +1,16 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 
 const TextWithImage = () => {
   return (
-    <div>TextWithImage</div>
+    <section className='container py-48 grid md:grid-cols-2'>
+        <h2 className='col-span-full'>About Us!</h2>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci dolorum est tenetur quam hic officia tempore, sequi obcaecati perferendis. Vel voluptates, reprehenderit nemo dicta exercitationem magnam perferendis optio. </p>
+          <p>Deleniti consequatur exercitationem nihil quis voluptatem. Deserunt ad assumenda iusto dolorem.</p>
+        </div>
+        <StaticImage src="../images/logo-colored.svg" alt="Twin Hawks Outdoors logo" />
+      </section>
   )
 }
 
