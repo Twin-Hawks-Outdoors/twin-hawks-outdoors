53357ac5a2e27334454848a58d125aa946bef318
Luke Miller <lukem1293@gmail.com>
2022-06-14 16:15:56 -0500

    modified gatsby-ssr

--- web/gatsby-ssr.tsx
+++ web/gatsby-ssr.tsx
@@ -1,2 +1,29 @@
-export { wrapPageElement } from './gatsby-browser';
-export { wrapRootElement } from './gatsby-browser';
+import { PortableTextComponentsProvider } from "@portabletext/react";
+import { AnimatePresence } from "framer-motion";
+import { CartProvider } from "use-shopping-cart";
+import Header from "./src/components/Header";
+import { portableTextComponents } from "./src/lib/portableTextComponents";
+import React from 'react'
+import { GatsbySSR } from "gatsby";
+export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({
+  element,
+}) => (
+  <PortableTextComponentsProvider components={portableTextComponents}>
+    <Header />
+    <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+    <Footer />
+  </PortableTextComponentsProvider>
+);
+
+export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({
+  element,
+}) => (
+  <CartProvider
+    cartMode="checkout-session"
+    stripe={process.env.STRIPE_PUBLIC_KEY || ""}
+    currency="USD"
+    language="en-US"
+  >
+    {element}
+  </CartProvider>
+);
