5b74042d2c720242d27e0b1cec6a0a05201229cd
Luke Miller <lukem1293@gmail.com>
2022-06-21 12:36:55 -0500

    added youtube schema to frontend of website

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,7 +2,15 @@ import { PortableText } from '@portabletext/react';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
-import { BsFacebook, BsInstagram, BsLinkedin, BsQuestionCircle, BsSnapchat, BsTwitter } from 'react-icons/bs';
+import {
+  BsFacebook,
+  BsInstagram,
+  BsLinkedin,
+  BsQuestionCircle,
+  BsSnapchat,
+  BsTwitter,
+  BsYoutube,
+} from 'react-icons/bs';
 import { FooterQuery } from '../../graphql-types';
 
 const iconMap = {
@@ -13,6 +21,7 @@ const iconMap = {
   snapchat: <BsSnapchat className="icon hover:text-[#FFFC00]" />,
   instagram: <BsInstagram className="icon hover:text-[#c13584]" />,
   linkedin: <BsLinkedin className="icon hover:text-[#2867b2]" />,
+  youtube: <BsYoutube />,
   default: <BsQuestionCircle className="icon hover:text-gray-100" />,
 };
 
@@ -61,11 +70,15 @@ function Footer() {
             <PortableText value={sanitySiteConfig?._rawFooterText} />
           </div>
         </div>
-        <nav className=' text-white'>
+        <nav className=" text-white">
           <ul className="flex gap-8 flex-wrap justify-between items-center">
             {sanitySiteConfig?.footerNavigation?.map((item) => (
-              <Link className="font-sans uppercase text-white "
-            activeClassName="text-red-500 border-b-current border-b-2" key={item?.id} to={item?.slug?.current as string}>
+              <Link
+                className="font-sans uppercase text-white "
+                activeClassName="text-red-500 border-b-current border-b-2"
+                key={item?.id}
+                to={item?.slug?.current as string}
+              >
                 {item?.page?.title}
               </Link>
             ))}
@@ -73,15 +86,20 @@ function Footer() {
         </nav>
         <div className="social-links flex gap-8 justify-center items-center">
           {sanitySiteConfig?.socialLinks?.map((item, index) => (
-              <a
-                key={item?._key || index}
-                target="_BLANK"
-                href={item?.url as string}
-                rel="noreferrer"
-                className="text-white focus:ring-offset-4 rounded-full focus:ring-offset-black group"
-              >
-                {iconMap[item?.platform  as keyof typeof iconMap?? 'default' as keyof typeof iconMap]}
-              </a>
+            <a
+              key={item?._key || index}
+              target="_BLANK"
+              href={item?.url as string}
+              rel="noreferrer"
+              className="text-white focus:ring-offset-4 rounded-full focus:ring-offset-black group"
+            >
+              {
+                iconMap[
+                  (item?.platform as keyof typeof iconMap) ??
+                    ('default' as keyof typeof iconMap)
+                ]
+              }
+            </a>
           ))}
         </div>
       </div>
