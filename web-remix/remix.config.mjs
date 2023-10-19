8c4d3c262c7f271db62fb41b83c14054f96d0a74
Luke Miller <lukem1293@gmail.com>
2023-10-19 16:41:56 -0500

    changed filename

---
+++ web-remix/remix.config.mjs
@@ -0,0 +1,21 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+	...baseConfig,
+	serverModuleFormat: 'cjs',
+  ignoredRouteFiles: ["**/.*"],
+  tailwind: true,
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+};
